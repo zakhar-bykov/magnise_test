9c9d8d2c4ce20be162bebe65a352fc174f258252
Zakhar Bykov <zak.bykov@gmail.com>
2022-05-24 13:30:24 +0300

    add: server.ts

---
+++ src/server.ts
@@ -0,0 +1,6 @@
+import config from './modules/config'
+import app from './express/app'
+
+const server = require('http').createServer(app)
+
+server.listen(config.server.port, () => console.log('Express server started on port: ' + config.server.port))
