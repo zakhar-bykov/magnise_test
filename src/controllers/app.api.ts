2a89bb635be285b2b539d0f0df0566bf916fb64f
Zakhar Bykov <zak.bykov@gmail.com>
2022-05-26 18:43:38 +0300

    add: app routes using services

--- src/controllers/app.api.ts
+++ src/controllers/app.api.ts
@@ -1,26 +1,39 @@
 import { Request as Req, Response as Res, NextFunction as Next } from 'express'
 
-import axios from '../modules/axios'
+import { AssetInfo } from '../types/responses'
+import { AssetDocument } from '../models/currencies'
+import currenciesService from '../service/currencies'
+
+import { CError } from '../types/error'
 
 
-export const getAllCryptoCurrencies = async (req: Req, res: Res, next: Next) => {
+export const getCryptoCurrenciesList = async (req: Req, res: Res, next: Next) => {
   try {
-    const apiRes = await axios.get('/assets')
-    res.json(apiRes.data)
+    const list: AssetDocument[] = await currenciesService.getCurrenciesList({ isCrypto: true })
+    res.json(list)
 
   } catch (err) {
     console.log(err)
-    res.status(500)
+
+    if (err instanceof CError)
+      res.status(err.code).send({ message: err.msg, details: err.details })
+    else
+      res.status(500)
   }
 }
 
-export const getCryptoCurrencies = async (req: Req, res: Res, next: Next) => {
+export const getCryptoCurrenciesInfo = async (req: Req, res: Res, next: Next) => {
   try {
-    const apiRes = await axios.get('/exchangerate/' + req.params.currencie + '?invert=false')
-    res.json(apiRes.data)
+    const currencies = req.params.currencies.split(/[,;\s]/)
+    const info: AssetInfo[] = await currenciesService.getCurrenciesInfo(currencies)
+    res.json(info)
 
   } catch (err) {
     console.log(err)
-    res.status(500)
+
+    if (err instanceof CError)
+      res.status(err.code).send({ message: err.msg, details: err.details })
+    else
+      res.status(500)
   }
 }
--- src/routes/app.api.ts
+++ src/routes/app.api.ts
@@ -5,8 +5,8 @@ const router: express.Router = express.Router()
 import * as api from '../controllers/app.api'
 
 
-router.get('/crypto-currencies', api.getAllCryptoCurrencies)
-router.get('/crypto-currencies/:currencie', api.getCryptoCurrencies)
+router.get('/crypto-currencies', api.getCryptoCurrenciesList)
+router.get('/crypto-currencies/:currencies', api.getCryptoCurrenciesInfo)
 
 //
 
