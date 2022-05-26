c6c18303d54be1d2141730348889a763b9e2e7ab
Zakhar Bykov <zak.bykov@gmail.com>
2022-05-26 18:44:11 +0300

    add: mongo connection

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,21 @@
 import config from './modules/config'
 import app from './express.app'
+import mongoose from './mongo.connection'
 
 const server = require('http').createServer(app)
 
+mongoose.connect(config.mongo.url + config.mongo.db)
 server.listen(config.server.port, () => console.log('Express server started on port: ' + config.server.port))
+
+
+/* import ws from './ws.connection'
+import wsHello from './types/ws.hello'
+
+console.log('ws')
+ws.on('open', () => {
+  ws.send( JSON.stringify(wsHello) )
+})
+
+ws.on('message', (data: Buffer) => {
+  console.log((data.toString()))
+}) */
